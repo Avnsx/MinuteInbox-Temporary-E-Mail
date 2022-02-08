02e78b97cd72f9d596d3bffb8db368646b09dcfb
Mika C <59329994+Avnsx@users.noreply.github.com>
2022-02-08 22:29:24 +0100

    Update minuteinbox.py

--- minuteinbox.py
+++ minuteinbox.py
@@ -2,11 +2,7 @@ import requests,json,random,re
 from time import sleep as s
 from bs4 import BeautifulSoup
 
-ua_list = []
-userlist=re.sub('\r\n', '\n', str(requests.get('http://pastebin.com/raw/VtUHCwE6').text)).splitlines()
-for x in userlist:ua_list.append(x)
-random.shuffle(ua_list)
-pers_UA=str(random.choice(ua_list))
+pers_UA = random.choice(tuple(set(re.sub('\r\n', '\n', requests.get('http://pastebin.com/raw/VtUHCwE6').text).splitlines())))
 min_sess = requests.Session()
 
 minuteinbox_headers = {
